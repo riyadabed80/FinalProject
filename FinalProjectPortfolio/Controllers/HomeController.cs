10fa01aaf1ef05219df1538b6989568f5a8c66c4
Timothy Snyder <snydertimothym@gmail.com>
2018-06-25 11:51:37 -0400

    Leaderboard Page

--- FinalProjectPortfolio/Controllers/HomeController.cs
+++ FinalProjectPortfolio/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinalProjectPortfolio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +7,12 @@ using System.Web.Mvc;
 
 namespace FinalProjectPortfolio.Controllers
 {
+    public class Leaders
+    {
+        public string UserName { get; set; }
+        public Portfolio UserPortfolio { get; set; }
+    }
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -24,6 +31,22 @@ namespace FinalProjectPortfolio.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Leaderboard()
+        {
+            portfolioEntities ORM = new portfolioEntities();
+            List<Portfolio> results = ORM.Portfolios.ToList();
+            List<Leaders> leaders = new List<Leaders>();
+
+            foreach (Portfolio portfolio in results)
+            {
+                string userName = ORM.AspNetUsers.Find(portfolio.userID).UserName;
+                leaders.Add(new Leaders() { UserPortfolio = portfolio, UserName = userName });
+            }
+
+            ViewBag.StockTable = leaders.OrderByDescending(x => x.UserPortfolio.totalprofiit).ToList();
             return View();
         }
     }
