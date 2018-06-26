c2133fcffebcc662a87220d30b30bbaae69f0840
Lovejoy-gitsit <26442317+Lovejoy-gitsit@users.noreply.github.com>
2018-06-26 11:50:34 -0400

    excluded weekends from calendar

---
+++ FinalProjectPortfolio/Scripts/JavaScript.js
@@ -0,0 +1,34 @@
+﻿
+//function rsfp_onSelectDate(date, type, args, calendar)
+//{
+//var dates = args[0];
+//    var date = dates[0];
+//    var year = date[0], month = date[1], day = date[2];
+    
+//    var d = new Date(year, month-1, day);
+//if (d.getDay() == 0 || d.getDay() == 1) {
+//        alert('Please select a date that is different from Monday or Sunday!');
+//    return false;
+//} else {
+//return true;
+//    }
+//    }
+//<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+//<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+//    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+//$(function () {
+//    $('#dateselector').datepicker({
+//        beforeShowDay: $.datepicker.noWeekends
+//    });
+//});
+
+$(function () {
+    $("#datepicker1, #datepicker2, #datepicker3").datepicker({
+        beforeShowDay: $.datepicker.noWeekends, 
+        minDate: '-20y',
+        maxDate: '0'
+    });
+});
+
+
+
